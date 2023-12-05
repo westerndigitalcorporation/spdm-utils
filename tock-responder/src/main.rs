a54096b3940e6c1794aeb66dedee00c3480bcde0
Alistair Francis <alistair.francis@wdc.com>
2023-12-05 12:55:00 +1000

    tock-responder: Initial commit of Tock responder support
    Add initial support for a Tock SPDM responder. This is an application
    that can run on Tock and handle SPDM MCTP packets.
    
    Currently this is in very early stages. The application runs, but does
    not yet act as a responder.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>

---
+++ tock-responder/src/main.rs
@@ -0,0 +1,105 @@
+// Licensed under the Apache License, Version 2.0 or the MIT License.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// Copyright (C) 2023, Western Digital Corporation or its affiliates.
+
+//! A simple libtock-rs SPDM example
+
+#![no_main]
+#![no_std]
+
+use core::ffi::{c_int, c_void};
+use core::fmt::Write;
+use critical_section::RawRestoreState;
+use embedded_alloc::Heap;
+use libspdm::libspdm_rs::{
+    SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P384,
+    SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA_384,
+};
+use libspdm::responder;
+use libspdm::spdm;
+use libtock::console::Console;
+use libtock::runtime::{set_main, stack_size};
+
+set_main! {main}
+stack_size! {0xE00}
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
+struct MyCriticalSection;
+critical_section::set_impl!(MyCriticalSection);
+
+unsafe impl critical_section::Impl for MyCriticalSection {
+    unsafe fn acquire() -> RawRestoreState {
+        // Tock is single threaded, so this can only be preempted by interrupts
+        // The kernel won't schedule anything from our app unless we yield
+        // so as long as we don't yield we won't concurrently run with
+        // other critical sections from our app.
+        // The kernel might schedule itself or other applications, but there
+        // is nothing we can do about that.
+    }
+
+    unsafe fn release(_token: RawRestoreState) {}
+}
+
+// Based on https://github.com/llvm/llvm-project/blob/main/compiler-rt/lib/builtins/bswapsi2.c
+#[no_mangle]
+pub extern "C" fn __bswapsi2(u: u32) -> u32 {
+    (((u) & 0xff000000) >> 24)
+        | (((u) & 0x00ff0000) >> 8)
+        | (((u) & 0x0000ff00) << 8)
+        | (((u) & 0x000000ff) << 24)
+}
+
+extern "C" {
+    // Provided by libtock-c
+    fn gettimeasticks(tv: *mut libspdm::libspdm_rs::timeval, tzvp: *mut c_void) -> c_int;
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn _gettimeofday(
+    tv: *mut libspdm::libspdm_rs::timeval,
+    tzvp: *mut c_void,
+) -> c_int {
+    gettimeasticks(tv, tzvp)
+}
+
+// Setup the heap and the global allocator.
+unsafe fn setup_heap() {
+    use core::mem::MaybeUninit;
+
+    const HEAP_SIZE: usize = 1024 * 64;
+    static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+    unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+}
+
+fn main() {
+    unsafe {
+        setup_heap();
+    }
+
+    writeln!(Console::writer(), "spdm-sample: app start\r",).unwrap();
+    let cntx_ptr = spdm::initialise_spdm_context();
+
+    // TODO: Add MCTP support to spdm-utils
+    //responder::register_device(cntx_ptr).unwrap();
+
+    unsafe {
+        spdm::setup_transport_layer(cntx_ptr).unwrap();
+    }
+    writeln!(
+        Console::writer(),
+        "spdm-sample: setup_transport_layer [ok]\r",
+    )
+    .unwrap();
+
+    responder::setup_capabilities(
+        cntx_ptr,
+        0,
+        None,
+        SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P384,
+        SPDM_ALGORITHMS_BASE_HASH_ALGO_TPM_ALG_SHA_384,
+    )
+    .unwrap();
+    writeln!(Console::writer(), "spdm-sample: setup_capabilities [ok]\r",).unwrap();
+}
