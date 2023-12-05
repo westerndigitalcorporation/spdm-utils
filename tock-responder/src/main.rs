2fc8e45643b9ef5dac465eff2bc89d32ce3887a5
Alistair Francis <alistair.francis@wdc.com>
2023-12-05 12:55:00 +1000

    tock-responder: Initial commit of Tock responder support
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    Signed-off-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>i

--- tock-responder/src/main.rs
+++ tock-responder/src/main.rs
@@ -69,7 +69,6 @@ pub unsafe extern "C" fn _gettimeofday(
 // Setup the heap and the global allocator.
 unsafe fn setup_heap() {
     use core::mem::MaybeUninit;
-
     const HEAP_SIZE: usize = 1024 * 64;
     static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
     unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
